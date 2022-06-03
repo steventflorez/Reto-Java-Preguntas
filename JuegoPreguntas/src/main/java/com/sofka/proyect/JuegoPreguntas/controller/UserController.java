acef676b91ca271b13eff617afd5b1ffa617e971
millergallegof <miller.gallegof@gmail.com>
2022-06-03 12:04:09 -0500

    creacion controlador usuario

---
+++ JuegoPreguntas/src/main/java/com/sofka/proyect/JuegoPreguntas/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.sofka.proyect.JuegoPreguntas.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", methods =
+        {RequestMethod.GET, RequestMethod.POST})
+public class UserController {
+
+    @GetMapping(path = "/user")
+    public void users() {
+
+    }
+
+    @PostMapping(path = "/user")
+    public void create() {
+        
+    }
+
+
+}
