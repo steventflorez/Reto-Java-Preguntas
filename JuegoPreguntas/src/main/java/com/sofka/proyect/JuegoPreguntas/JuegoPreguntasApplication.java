add9d1d898ef881146b25199ce44565110fe9d60
Stevent Florez <steventflorez94@gmail.com>
2022-06-03 15:35:04 +0200

    primer commit

---
+++ JuegoPreguntas/src/main/java/com/sofka/proyect/JuegoPreguntas/JuegoPreguntasApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.proyect.JuegoPreguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoPreguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoPreguntasApplication.class, args);
+	}
+
+}
---
+++ JuegoPreguntas/src/test/java/com/sofka/proyect/JuegoPreguntas/JuegoPreguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.proyect.JuegoPreguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoPreguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
